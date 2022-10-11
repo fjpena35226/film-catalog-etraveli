4f0f706332f4d19068692de40ea8e5f3dfc3eb9f
fjpena35226 <fjpena35226@gmail.com>
2022-10-11 12:44:06 +0200

    Creating home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import 'styles/globals.css'
+import { AlertProvider } from 'src/context/alert.context'
+
+const queryClient = new QueryClient()
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AlertProvider>
+        <Component {...pageProps} />
+      </AlertProvider>
+    </QueryClientProvider>
+  )
 }
 
-export default MyApp
+export default App
