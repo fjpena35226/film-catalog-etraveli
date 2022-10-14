0ace56d59c66667ddfcffc600921684b3191ff92
fjpena35226 <fjpena35226@gmail.com>
2022-10-14 12:59:26 +0200

    Setting light mode by default

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+})
 
 import 'styles/globals.css'
 import { AlertProvider } from 'src/context/alert.context'
@@ -9,9 +17,12 @@ const queryClient = new QueryClient()
 function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <AlertProvider>
-        <Component {...pageProps} />
-      </AlertProvider>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <AlertProvider>
+          <Component {...pageProps} />
+        </AlertProvider>
+      </ThemeProvider>
     </QueryClientProvider>
   )
 }
